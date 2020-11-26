067f10cb908f9209d6b0e2098f28e4c2c7e947fd
np9603 <np9603@g.rit.edu>
2020-11-26 15:52:23 -0500

    Added GCD

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -1,5 +1,29 @@
 public class GreatestCommonDivisor {
-    public static void main(String[] args) {
 
+    public static int getGreatestCommonDivisor(int first, int second){
+        if(first < 10 || second < 10){
+            return -1;
+        }
+        else{
+            int gcd = 1;
+            int divisor = 1;
+            while(divisor<=first && divisor<=second){
+                divisor++;
+                if(first%divisor==0 && second%divisor==0){
+                    gcd = divisor;
+                }
+                else
+                    continue;
+            }
+            return gcd;
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(9,18));
+        System.out.println(getGreatestCommonDivisor(81,153));
+    }
+
 }
