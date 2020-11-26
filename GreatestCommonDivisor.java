f635f8d06c1d282fe82e491e12ce5210ac8d0b80
np9603 <np9603@g.rit.edu>
2020-11-26 12:15:33 -0500

    Trying rollback

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -1,27 +1,5 @@
-import java.sql.SQLOutput;
-
 public class GreatestCommonDivisor {
-    public static int getGreatestCommonDivisor(int first, int second){
-        if(first<10 || second<10)
-            return -1;
-        else {
-            int smallerOfTwo = first < second ? first : second;
-            int gcd = 1;
-            for (int i = 1; i < smallerOfTwo; i++) {
-                if (first % i == 0 && second % i == 0)
-                    gcd = i;
-                else
-                    continue;
-            }
-            return gcd;
-        }
-
-    }
     public static void main(String[] args) {
-        System.out.println(getGreatestCommonDivisor(25,15));
-        System.out.println(getGreatestCommonDivisor(12,30));
-        System.out.println(getGreatestCommonDivisor(9,18));
-        System.out.println(getGreatestCommonDivisor(81,153));
 
     }
 }
